f6c0568034c2bdf2fc352af7417ab5b4a55e3b47
Rodrigo Moraes <78232612+rodrigomoraes28@users.noreply.github.com>
2022-10-20 22:32:05 -0300

    Update Questao01.jsx

--- web_prova _finalizado/src/components/questao1/Questao01.jsx
+++ web_prova _finalizado/src/components/questao1/Questao01.jsx
@@ -1,16 +1,13 @@
-import react from 'react'
-import Questao01A from './Questao01A';
-import Questao01B from './Questao01B';
+import React from 'react'
 
 
 function Questao01(props){
   return(
       <div>
           <h1>Questão 01.</h1> <br />
-          <Questao01A nome={props.nome} sobrenome={props.sobrenome} curso={props.curso}/>
-          <Questao01B disciplinas={props.disciplinas}/>
+          {props.children}
       </div>
   )
 }
 
-export default Questao01
+export default Questao01;
