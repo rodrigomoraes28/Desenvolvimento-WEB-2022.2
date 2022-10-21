5e9a78cc2e6906d7fba6618777461939346a9d0d
Rodrigo Moraes <78232612+rodrigomoraes28@users.noreply.github.com>
2022-10-20 22:40:07 -0300

    Update Questao01B.jsx

--- web_prova _finalizado/src/components/questao1/Questao01B.jsx
+++ web_prova _finalizado/src/components/questao1/Questao01B.jsx
@@ -1,17 +1,29 @@
-import react from 'react'
+import React from 'react'
 
-function Disciplinas(props) {
-  return <li>{ props.disciplinas }</li>;
+
+const Questao01B = () => {
+
+    
+    let Disciplinas = ["PI", "PDS", "WEB", "FBD"]
+
+
+    const listarDisciplinas = () => {
+        return Disciplinas.map(
+            (disciplina, index) => {
+
+                return(
+                    <h3> {disciplina} </h3>
+                )
+            }
+        )
+    }
+
+    return (
+        <div>
+            <h3 className="text-primary" > Disciplinas: </h3>
+            {listarDisciplinas()}
+        </div>
+    )
 }
 
-function Questao01B(props){
-  return(
-      <div>
-          <h3>Disciplinas: </h3>
-          <ul>
-              {props.disciplinas.map((disciplinas) => <Disciplinas disciplinas={disciplinas} />)}
-          </ul>
-      </div>
-  )
-}
-export default Questao01B
+export default Questao01B;
